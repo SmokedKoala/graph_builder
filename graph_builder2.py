e437303f47c6f2df768bfa6f0b5ce85d240a4a08
SmokedKoala <k.azarov@inbox.ru>
2020-09-14 19:28:21 +0300

    Working Project
    v.1.0.0

--- graph_builder2.py
+++ graph_builder2.py
@@ -1,21 +1,19 @@
 
 import requests
 import re
-import json
-import pydot
 from graphviz import Digraph
-from graphviz import Source
 import os
 
 # были проблемы с PATH, добавил вручную
 os.environ["PATH"] += os.pathsep + 'C:\\Program Files (x86)\\Graphviz 2.28\\\bin'
 
+# словарь для пакетов и их зависимостей
 package_dict = {}
+
 # функция парсинга JSON
 def json_parse(package_name):
     request =requests.get(f'https://pypi.org/pypi/'+package_name+'/json')
     package_dict[package_name]=[]
-    # print(request.json()["info"]['requires_dist'])
     # проверка, есть ли необходимые пакеты
     if not request.json()["info"]['requires_dist'] == None:
         # добавление необходимых пакетов в словарь
@@ -23,11 +21,13 @@ def json_parse(package_name):
             if not re.search(r'extra',dependence_name):
                 dependence_name=re.sub('[^a-zA-Z_-]', '', dependence_name)
                 package_dict[package_name].append(dependence_name)
-        # рекурсивный вызов функции для следующих пакетов
+        # рекурсивный вызов функции для пакетов, зависимых от текущего 
         for name in package_dict[package_name]:
             if not name in package_dict and requests.get(f'https://pypi.org/pypi/'+name+'/json').status_code == 200:
                 json_parse(name)
-#   функция создания графа   
+
+
+#   функция вывода кода для графа   
 def graph_creation():
     dot = Digraph()
     for key in package_dict.keys():
@@ -35,13 +35,8 @@ def graph_creation():
         for name in package_dict[key]:
             dot.edge(key,name)
     print(dot.source)
-    dot.render()
 
 
 package_name = str(input()).lower()
 json_parse(package_name)
-# for _ in package_dict:
-    # if package_dict[item_key] == []:
-        # package_dict.pop(item_key)
-# print(package_dict)
 graph_creation()
